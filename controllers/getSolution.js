4ae43ac33a3ab5f144d5189f77eac8087804a45e
Matt Wiseman <mrslwiseman@gmail.com>
2017-11-11 09:06:46 +1100

    fix regex anomalies

--- controllers/getSolution.js
+++ controllers/getSolution.js
@@ -2,7 +2,7 @@ const parse = require('url-parse');
 
 module.exports = (url) => {
   const data = parse(url, true);
-  const challenge = data.pathname.replace(/\/challenges\//, '');
+  const challenge = data.pathname.replace(/\/challenges\//, '').replace(/[^a-zA-Z0-9_%]%20/g, '').replace(/%20/g, '-');
   const solution = data.query.solution.replace(/fccss/, '<script>').replace(/fcces/, '</script>');
   return {
       challenge,
